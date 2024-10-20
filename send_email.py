d5b860dd35e8d736e86d494af3c70e17fe458c9d
Ping <14463ow@gmail.com>
2024-10-20 15:42:38 -0700

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,22 +1,3 @@
-# import smtplib, ssl
-# import os
-
-
-# port = 465
-# smtp_server = "smtp.gmail.com"
-# USERNAME = os.environ.get('USER_EMAIL')
-# PASSWORD = os.environ.get('USER_PASSWORD')
-# message = """\
-# Subject: GitHub Email Report
-
-# This is your daily email report.
-# """
-
-# context = ssl.create_default_context()
-# with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-#     server.login(USERNAME,PASSWORD)
-#     server.sendmail(USERNAME,USERNAME,message)
-
 import smtplib, ssl
 import os
 import requests
@@ -48,4 +29,4 @@ context = ssl.create_default_context()
 # Sending the email
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(USERNAME, PASSWORD)
-    server.sendmail(USERNAME, USERNAME, message)  # Send email to yourself (or modify as needed)
+    server.sendmail(USERNAME, USERNAME, message.encode('utf-8'))  # Encode message as UTF-8
